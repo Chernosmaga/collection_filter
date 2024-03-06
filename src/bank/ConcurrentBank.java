0bba4a29a4f400da8884c3acb6f64966c0282c75
Chernosmaga <sergeychernosmaga1997@yandex.ru>
2024-03-06 12:36:32 +0300

    fix: добавил семафор для доступа к потокам

--- src/bank/ConcurrentBank.java
+++ src/bank/ConcurrentBank.java
@@ -3,9 +3,11 @@ package bank;
 import exception.TransactionException;
 
 import java.util.concurrent.ConcurrentLinkedQueue;
+import java.util.concurrent.Semaphore;
 
 public class ConcurrentBank {
     private ConcurrentLinkedQueue<BankAccount> accounts = new ConcurrentLinkedQueue<>();
+    private Semaphore semaphore = new Semaphore(1);
 
     public BankAccount createAccount(int amount) {
         BankAccount bankAccount = new BankAccount();
@@ -15,12 +17,19 @@ public class ConcurrentBank {
     }
 
     public void transfer(BankAccount account1, BankAccount account2, int amount) {
-        synchronized (this) {
+        try {
+            semaphore.acquire();
             if (account1.getBalance() - amount < 0) {
                 throw new TransactionException("Ошибка транзакции");
             }
             account1.setAmount(account1.getBalance() - amount);
+            System.out.println(account1.getBalance());
             account2.setAmount(account2.getBalance() + amount);
+            System.out.println(account2.getBalance());
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        } finally {
+            semaphore.release();
         }
     }
 
